e28dba1fb1cafafaa0879685fb0a036df879c85a
SrivathsanSivakumar <sri.skumar05@gmail.com>
2024-07-31 15:56:20 -0400

    dataset pulling refactor

--- prepare_model_and_data.py
+++ prepare_model_and_data.py
@@ -200,21 +200,6 @@ def main():
     args = get_args()
 
     dataset_path = "data/OACE"
-    data_download_path = "data/OACE.tar.gz"
-
-    # get dataset from google drive
-    if os.path.isdir(dataset_path):
-        print("Dataset already exists")
-    else:
-        if os.path.exists(data_download_path):
-            with tarfile.open(data_download_path, "r:gz") as tar:
-                tar.extractall(path="data")
-        else:
-            download_url = "https://drive.google.com/uc?export=download&id=1Qzuf3M7GOi5_JCmvHopTIe_G4IO7-hjP"
-            gdown.download(download_url, data_download_path, quiet=False)
-            with tarfile.open(data_download_path, "r:gz") as tar:
-                    tar.extractall(path="data")
-                    
     criterion = nn.CrossEntropyLoss() # to learn both classes and not just one. 
 
     if args.train:
